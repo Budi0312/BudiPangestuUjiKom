72d68ee16b1f08c0466ae5c27e2cabcc8586fc5b
Budi0312 <72286755+Budi0312@users.noreply.github.com>
2025-11-20 10:02:36 +0700

    Initial commit

---
+++ Assets/Script/tombolPolos.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tombolPolos : MonoBehaviour
+{
+public bool sedangPutar;
+    public AudioSource sfxPlay;
+    //public AudioSource sfxCredit;
+    public AudioSource sfxExit;
+    public AudioSource sfxMenu;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void aksiPlay()
+    {
+        StartCoroutine(TungguSuara());
+        //sfxPlay.Play();
+        SceneManager.LoadScene("ariefPemain");
+    }
+
+    public void aksiCredit()
+    {
+        //sfxCredit.Play();
+        SceneManager.LoadScene("credit");
+    }
+
+    public void aksiExit()
+    {
+        sfxExit.Play();
+        Application.Quit();
+    }
+    public void aksiMenu()
+    {
+         sfxMenu.Play();
+        SceneManager.LoadScene("ariefMenu");
+    }
+
+    public IEnumerator TungguSuara()
+    {
+        sedangPutar=true;
+        putarSuara();
+        yield return new WaitWhile (()=>sedangPutar);
+       
+    }
+
+    public void putarSuara()
+    {
+        sfxPlay.Play();
+         Debug.Log("Tunggu");
+        sedangPutar =false;
+    }
+}
