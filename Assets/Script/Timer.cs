e0a462748c6b9b592f5cb5f760556ce380b0d8b6
Budi0312 <72286755+Budi0312@users.noreply.github.com>
2025-11-20 10:50:48 +0700

    Script Timer

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.VisualScripting;
+//using Microsoft.Unity.VisualStudio.Editor;
+public class timer : MonoBehaviour
+{
+    public float time = 60f;
+    public TMP_Text timerText;
+    public bool isCounting = true;
+    public objPemain objPemain;
+
+    private Coroutine timerCoroutine;
+
+    public Image ProgressFill;
+
+    private void Start()
+    {
+        // tampilkan waktu awal
+        //timerText.text = "Waktu : " + time.ToString();
+
+        // mulai hitung mundur otomatis
+        //timerCoroutine = StartCoroutine(HitungTimer());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+   /* IEnumerator HitungTimer()
+    {
+        while (isCounting && time > 0)
+        {
+            yield return new WaitForSeconds(1f);
+
+            time -= 1;
+            timerText.text = "Waktu : " + time.ToString();
+        }
+
+        // jika waktu habis
+        if (time <= 0)
+        {
+            isCounting = false;
+            timerText.text = "Waktu : 0";
+            PlayerPrefs.SetInt("skor", objPemain.skor);
+            PlayerPrefs.Save();
+            objPemain.simpanHightSkor();
+            SceneManager.LoadScene("ariefYouWin");
+        }
+    }*/
+
+    IEnumerator HitungTimer()
+    {
+        while(isCounting==true && time<time)
+        {
+            time=time+1;
+            //TextTimer.text=Waktu.ToString();
+            ProgressFill.fillAmount=time/time;
+            yield return new WaitForSeconds(1);
+        }
+
+    }
+}
