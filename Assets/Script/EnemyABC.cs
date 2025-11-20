247653b66bfc2a9e9184e12d7d60aad6618a8160
Budi0312 <72286755+Budi0312@users.noreply.github.com>
2025-11-20 11:25:56 +0700

    spawnscript

---
+++ Assets/Script/EnemyABC.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyABC : MonoBehaviour
+{
+          void OnCollisionEnter2D(Collision2D col)
+    {
+        // ❌ Tidak hancur bila menyentuh pemain
+        if (col.collider.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<objPemain>().KurangNyawa();
+        }
+
+        // ❌ Tidak hancur bila menyentuh objek lain selain lantai
+        if (!col.collider.CompareTag("Lantai"))
+        {
+            
+        }
+
+        // ✅ HANCUR hanya ketika menyentuh lantai
+                Destroy(gameObject);
+
+    }
+}
